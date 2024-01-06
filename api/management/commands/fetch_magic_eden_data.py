285afc2c67cdfcaef7593f591147660051a20c10
Stian Lystad <Stianlystad_1@hotmail.com>
2024-01-06 02:01:26 +0000

    more api testing

--- api/management/commands/fetch_magic_eden_data.py
+++ api/management/commands/fetch_magic_eden_data.py
@@ -9,6 +9,8 @@ class Command(BaseCommand):
         url = "https://api-mainnet.magiceden.dev/v2/ord/btc/collections/nodemonkes"
         headers = {"accept": "application/json"}
         response = requests.get(url, headers=headers)
+        print("Response status code:", response.status_code)
+        print("Response content:", response.text)
         if response.status_code == 200:
             return response.json()
         else:
@@ -24,7 +26,19 @@ class Command(BaseCommand):
         else:
             # Handle errors
             return None
-    
+
+    def fetch_new_listings(self, collection_symbol, kind="list", limit=10):
+        url = f"https://api-mainnet.magiceden.dev/v2/ord/btc/activities?collectionSymbol={collection_symbol}&kind={kind}&limit={limit}"
+        headers = {"accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        print("Response status code:", response.status_code)
+        print("Response content:", response.text)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            # Handle errors
+            return None
+    '''
     def fetch_token_data(self, token_ids, collection_symbol, sort_by='priceAsc', show_all=True):
         url = f"https://api-mainnet.magiceden.dev/v2/ord/btc/tokens?tokenIds={token_ids}&collectionSymbol={collection_symbol}&showAll={show_all}&sortBy={sort_by}"
         headers = {"accept": "application/json"}
@@ -35,7 +49,7 @@ class Command(BaseCommand):
         else:
             # Handle errors
             return None
-
+'''
     def handle(self, *args, **kwargs):
         collection_data = self.fetch_collection_data()
         collection_stats = self.fetch_collection_stats()
@@ -50,8 +64,13 @@ class Command(BaseCommand):
             print("Collection Stats:", collection_stats)
             self.stdout.write(self.style.SUCCESS('Successfully fetched collection stats'))
 
+        new_listings = self.fetch_new_listings('nodemonkes')
+        if new_listings:
+            print("New Listings Data:", new_listings)
+            self.stdout.write(self.style.SUCCESS('Successfully fetched new listings'))
+    '''
         token_data = self.fetch_token_data('7034', 'nodemonkes')
         if token_data:
             print("Token Data:", token_data)
-        self.stdout.write(self.style.SUCCESS('Successfully fetched token data'))
-
+            self.stdout.write(self.style.SUCCESS('Successfully fetched token data'))
+        '''
